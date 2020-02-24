3801167198d1b838ed3ba5352251d8da45e618d7
Святослав <55986619+arisviel@users.noreply.github.com>
2020-02-24 12:37:54 +0300

    Add files via upload

---
+++ face_recognition/Recognizing.py
@@ -0,0 +1,30 @@
+import numpy as np
+import face_recognition
+import os
+
+r = sys.argv[1]
+w = int(sys.arfv[2])
+
+save_root = '/home/eatmystyle/Pictures/features' # direction to features file
+features = np.loadtxt(save_root, delimiter=',') # read our features
+
+img = face_recognition.load_image_file(r)
+
+enc= face_recognition.face_encodings(img) #encode faces
+if np.array(enc).shape[0] < 1: # no faces ddetected
+    print("I wasn't able to locate any faces in the image. Check the image files. Aborting...")
+    os.write(w, b'0')
+    quit()
+elif np.array(enc).shape[0] > 1: # more than 1 face detected
+    print("Too many faces detected. Check the image files. Aborting...")
+    os.write(w, b'0')
+    quit()
+
+answer = face_recognition.compare_faces(enc, features) # predictions
+
+if np.sum(np.array(answer)) == 1:
+    os.write(w, b'1')
+else:
+    os.write(w, b'0')
+quit()
+
